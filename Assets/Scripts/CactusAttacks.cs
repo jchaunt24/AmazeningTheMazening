af83d6acb1889dc62a88548cf3450ea3b1f1e3a4
jchaunt24 <76946258+jchaunt24@users.noreply.github.com>
2024-03-21 14:27:51 -0400

    Cycle 7 Day 4

--- Assets/Scripts/CactusAttacks.cs
+++ Assets/Scripts/CactusAttacks.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class CactusAttacks : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject projectile;
+    Vector3 explodePosition;
+    public float timer;
+    public float spawnTime = .5f;
+    float thrust = 50f;
+    void Update(){
+        timer += Time.deltaTime;
+        if(timer >= spawnTime){
+            explodePosition = transform.position;
+            Debug.Log("Kaboom");
+            Vector3 offset = new Vector3(0,0,0);
+            offset.x = -2;
+            var explodingparticle = Instantiate(projectile,explodePosition +offset, projectile.transform.rotation);
+            explodingparticle.GetComponent<Rigidbody2D>().AddForce(transform.right * -thrust);
+            explodingparticle.GetComponent<Rigidbody2D>().AddTorque(50);
+            offset.x = 2;
+            explodingparticle = Instantiate(projectile,explodePosition + offset, projectile.transform.rotation);
+            explodingparticle.GetComponent<Rigidbody2D>().AddForce(transform.right * thrust);
+            explodingparticle.GetComponent<Rigidbody2D>().AddTorque(50);
+            offset.x = 0;
+            offset.y = -2;
+            explodingparticle = Instantiate(projectile,explodePosition + offset, projectile.transform.rotation);
+            explodingparticle.GetComponent<Rigidbody2D>().AddForce(transform.up * -thrust);
+            explodingparticle.GetComponent<Rigidbody2D>().AddTorque(50);
+            offset.y = 2;
+            explodingparticle = Instantiate(projectile,explodePosition+ offset, projectile.transform.rotation);
+            explodingparticle.GetComponent<Rigidbody2D>().AddForce(transform.up * thrust);
+            explodingparticle.GetComponent<Rigidbody2D>().AddTorque(50);
+            
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            // Diagonals
+            offset.x = -2;
+            offset.y = 2;
+            explodingparticle.GetComponent<Rigidbody2D>().AddForce(transform.right * -thrust);
+            explodingparticle.GetComponent<Rigidbody2D>().AddTorque(50);
+            offset.x = 2;
+            offset.y = -2;
+            explodingparticle = Instantiate(projectile,explodePosition + offset, projectile.transform.rotation);
+            explodingparticle.GetComponent<Rigidbody2D>().AddForce(transform.right * thrust);
+            explodingparticle.GetComponent<Rigidbody2D>().AddTorque(50);
+            offset.y = -2;
+            offset.x = 2;
+            explodingparticle = Instantiate(projectile,explodePosition + offset, projectile.transform.rotation);
+            explodingparticle.GetComponent<Rigidbody2D>().AddForce(transform.up * -thrust);
+            explodingparticle.GetComponent<Rigidbody2D>().AddTorque(50);
+            offset.y = 2;
+            offset.x = -2;
+            explodingparticle = Instantiate(projectile,explodePosition+ offset, projectile.transform.rotation);
+            explodingparticle.GetComponent<Rigidbody2D>().AddForce(transform.up * thrust);
+            explodingparticle.GetComponent<Rigidbody2D>().AddTorque(50);
+            timer = 0;
+        }
     }
 }
